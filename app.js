e8cb0b7d01e3a1217a48a9766ac13d6dbc36ce10
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-11-21 16:40:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const buttons = document.querySelector("button");
+const h1 = document.querySelector("h1");
 
 buttons.addEventListener("click", () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
 
-  const colors = `rgb(${r},${g},${b})`;
+  const colors = `rgb(${r}, ${g}, ${b})`;
   document.body.style.backgroundColor = colors;
 
   h1.innerText = colors;
 });
-
-const h1 = document.querySelector("h1");
